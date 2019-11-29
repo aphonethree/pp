c3bcabbd87284ae365f0525496bddf8b5f7c594d
aphonethree <7638223abc@gmail.com>
2019-11-29 18:51:18 +0800

    modified:   test.c

--- test.c
+++ test.c
@@ -4,15 +4,19 @@
 int main(int argc,char* argv[])
 {
     int *min_local;
-    int rank_number,cpu_number;
+    int rank_number,cpu_number,tag;
     int *min_global,local_min=5;
     min_global = (int *)malloc(8*sizeof(int));
     min_local = (int *)malloc(8*sizeof(int));
+    int *vector_swap_forward = (int*)malloc(1*8*sizeof(int));
+     int *read_buf_back = (int*)malloc(8*sizeof(int));
+     MPI_Request request;
+    MPI_Status status;
       MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_number);
     MPI_Comm_size(MPI_COMM_WORLD, &cpu_number);
     for(int i=0;i<8;i++)
-        min_global[i] = i;
+       vector_swap_forward[i]=i;
     if(rank_number==1)
         local_min=10;
     if(rank_number==2)
@@ -23,6 +27,12 @@ int main(int argc,char* argv[])
    	 for(int i=0;i<cpu_number;i++)
         	printf("%d  \n",min_global[i]);
 	}
+    if(rank_number==1){
+        MPI_Isend(vector_swap_forward,8,MPI_INT,2,tag,MPI_COMM_WORLD,&request);
+    }
+    if(rank_number==2){
+        MPI_Irecv(read_buf_back,W,MPI_INT,1,tag,MPI_COMM_WORLD,&request);
+    }
         MPI_Finalize();
         return 0;
 }
