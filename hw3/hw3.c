d0010f4b79d2788b74ef0861a982a1948f570e1d
aphonethree <7638223abc@gmail.com>
2019-11-29 11:15:15 +0800

    modified:   hw3.c

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
   int vector_swap_backword = malloc(1*W*sizeof(int));
   int read_buf_front = malloc(1*W*sizeof(int));
   int read_buf_back = malloc(1*W*sizeof(int));
-  int local_l,tag=0;
+  int local_l,tag=0,min=0;
   MPI_Request rerquest;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank_number);
@@ -86,13 +86,13 @@ int main(int argc, char **argv) {
             }
         }
         if(rank_number==0){
-            MPI_Isend(&vector_swap_backword,1,MPI_INT,rank_number+1,MPI_COMM_WORLD,&reques);
+            MPI_Isend(&vector_swap_backword,1,MPI_INT,rank_number+1,tag,MPI_COMM_WORLD,&reques);
             MPI_Irecv(&rerad_buf_back,1,MPI_INT,rank_number+1,tag,MPI_COMM_WORLD,&request);
             for(int i=0;i<W;i++)
                 next[(local_l*rank_number+local_l)*W+i] = read_buf_back[i];
         }else if(rank_number>0 && rank_number<cpu_number){
-            MPI_Isend(&vector_swap_forward,1,MPI_INT,rank_number-1,MPI_COMM_WORLD,&request);
-            MPI_Isend(&vector_swap_backword,1,MPI_INT,rank_number+1,MPI_COMM_WORLD,&reques);
+            MPI_Isend(&vector_swap_forward,1,MPI_INT,rank_number-1,tag,MPI_COMM_WORLD,&request);
+            MPI_Isend(&vector_swap_backword,1,MPI_INT,rank_number+1,tag,MPI_COMM_WORLD,&reques);
             MPI_Irecv(&rerad_buf_back,1,MPI_INT,rank_number+1,tag,MPI_COMM_WORLD,&request);
             MPI_Irecv(&rerad_buf_front,1,MPI_INT,rank_number-1,tag,MPI_COMM_WORLD,&request);
             MPI_Wait(&request,&status);
@@ -101,7 +101,7 @@ int main(int argc, char **argv) {
                 next[(local_l*rank_number+local_l)*W+i] = read_buf_back[i];
             }
         }else if(rank_number==cpu_number){
-            MPI_Isend(&vector_swap_forward,1,MPI_INT,rank_number-1,MPI_COMM_WORLD,&request);
+            MPI_Isend(&vector_swap_forward,1,MPI_INT,rank_number-1,tag,MPI_COMM_WORLD,&request);
             MPI_Irecv(&rerad_buf_front,1,MPI_INT,rank_number-1,tag,MPI_COMM_WORLD,&request);
             MPI_Wait(&request,&status);
              for(int i=0;i<W;i++)
